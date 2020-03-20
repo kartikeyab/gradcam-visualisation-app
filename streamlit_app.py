902c0f6c74f921e59057ecd8857fcfdae1c688ba
Kartikeya <kartikeya.bhardwaj@jocata.com>
2020-03-20 13:30:14 +0530

    Added text to main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,14 @@ import numpy as np
 from PIL import Image
 
 
-st.title("GradCam Visualisations")
+st.title("VGG GradCam Visualisations")
+
+st.write("This is a webapp which let's you visualise the Gradient Weighted Class Activation Mappings (Grad-Cam) of a \n"
+        "pre-trained VGG16 Convolutional Neural Net, trained on the ImageNet dataset. You also have the\n"
+        "flexibility to select a specific layer of the model which will change the mappings accordingly. \n"
+        "The output of the model is a heatmap which  highlights the pixels in the image which are \n"
+        "picked up by the model to classify the given image into a certain class.")
+
 
 def preprocess(img, req_size = (224,224)):
     image = Image.fromarray(img.astype('uint8'))
@@ -23,11 +30,11 @@ def preprocess(img, req_size = (224,224)):
     return face_array
 
 #Face image upload
-upload_image = st.file_uploader("Choose the image file")
+upload_image = st.file_uploader("Upload Image")
 if upload_image is not None:
     file_bytes = np.asarray(bytearray(upload_image.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
-    st.image(opencv_image, channels="BGR",use_column_width=True)
+    st.image(opencv_image, width = 270, channels="BGR",use_column_width=True)
     opencv_image_processed = preprocess(opencv_image)
 
 #load model
@@ -58,13 +65,13 @@ if st.button('Visualise'):
     (heatmap, output) = cam.overlay_heatmap(heatmap, opencv_image, alpha=0.5)
 
     # draw the predicted label on the output image
-    cv2.rectangle(output, (0, 0), (340, 40), (0, 0, 0), -1)
+    cv2.rectangle(output, (0, 0), (340,30), (0, 0, 0), -1)
     cv2.putText(output, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
         0.8, (255, 255, 255), 2)
 
     # display the original image and resulting heatmap and output image
     # to our screen
-    output = np.hstack([opencv_image, heatmap, output])
+    output = np.hstack([heatmap, output])
     output = imutils.resize(output, height=2700)
     st.image(output, channels="BGR",use_column_width=True)
     
